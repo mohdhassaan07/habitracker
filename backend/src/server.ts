2a0fe92ffd12da05e4a43b4657cef2edb1a710b1
hassaan <mohdhassaan837@gmail.com>
2026-08-21 18:07:01 +0530

    fixed prod error

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,6 @@
-import { server, PORT } from "./app";
+import { server } from "./app";
+
+const PORT = process.env.PORT || 3001;
 
 server.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
